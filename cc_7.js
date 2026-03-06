2cb9c9891f3825caaa817388589b8e4b90c53f54
Charles <cholzschuher@usf.edu>
2026-03-06 15:47:35 -0500

    DOM API

--- cc_7.js
+++ cc_7.js
@@ -1,3 +1,10 @@
 let headlineInput = document.getElementById('headline-input');
 let updateButton = document.getElementById('update-btn');
 let ctaHeadline = document.getElementById('cta-headline');
+
+updateButton.addEventListener('click', () => {
+    let newText = headlineInput.value;
+    
+        ctaHeadline.textContent = newText;
+        headlineInput.value = "";
+});
